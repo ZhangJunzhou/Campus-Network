1422dea98fbc0340ea207032acf232e39d6a3ac3
ZhangJunzhou <52271504+ZhangJunzhou@users.noreply.github.com>
2019-10-10 16:55:08 +0800

    Create logout_url.py

---
+++ logout_url.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+from urllib import request
+from urllib.request import urlopen
+import urllib
+
+def logout():
+    posturl="http://10.3.8.211/logout"
+    header={"User-Agent":"Mozilla/5.0 (compile;MSIE 10.0; Windows NT 6.1; Trident/6.0)",
+    "Referer":"http://10.3.8.211/F.html"
+        }
+    req = request.Request(posturl,headers=header)
+    try:
+        response = urlopen(req)
+        print("Logout success")
+    except urllib.error.URLError as e:
+        print("Logout failed")
+
+if __name__ == '__main__':
+    logout()
